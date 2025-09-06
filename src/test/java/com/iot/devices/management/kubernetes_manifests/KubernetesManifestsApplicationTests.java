775ae2404b68761c2b99ad34c3373cd9354169a0
Ihor <53528174+ihorlapka@users.noreply.github.com>
2025-09-06 14:53:16 +0300

    Add files via upload
    init commit

---
+++ src/test/java/com/iot/devices/management/kubernetes_manifests/KubernetesManifestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iot.devices.management.kubernetes_manifests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesManifestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
